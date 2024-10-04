7c216b45083d63cd1cc86ceb87f42cbd25129d1f
JACY24 <jordcluitmans@gmail.com>
2024-10-04 15:15:28 +0200

    Added method to calculate distance

--- simulation/simulation.py
+++ simulation/simulation.py
@@ -2,8 +2,9 @@ import scenic
 
 from scenic.simulators.newtonian import NewtonianSimulator
 from scenic.domains.driving.roads import Network
+import shapely as shapely
 
-NUM_SIMULATIONS = 5
+NUM_SIMULATIONS = 3
 CAR_WIDTH = 2
 CAR_LENGTH = 4.5
 SCENARIO = scenic.scenarioFromFile('simulation/test.scenic',
@@ -11,7 +12,7 @@ SCENARIO = scenic.scenarioFromFile('simulation/test.scenic',
                                     mode2D=True)
 
 def exec_simulation(network = Network.fromFile('Scenic/assets/maps/CARLA/Town05.xodr')):
-    simulator = NewtonianSimulator(network, render=False)
+    simulator = NewtonianSimulator(network, render=True)
 
     # Execute NUM_SIMULATIONS simulations and evaluate them
     scene, _ = SCENARIO.generate()
@@ -31,17 +32,30 @@ def exec_simulation(network = Network.fromFile('Scenic/assets/maps/CARLA/Town05.
 
     return collided, simulation.result.records
 
+def calc_distance(a, b):
+    poly_a = shapely.Polygon(a[0:3])
+    poly_b = shapely.Polygon(b[0:3])
+
+    return shapely.distance(poly_a, poly_b)
+
 def main():
-    collisions, results = [], []
+    collisions, results, distances = [], [], []
 
     for i in range(NUM_SIMULATIONS):
         collided, simulation_result = exec_simulation()
         collisions.append(collided)
         results.append(simulation_result)
 
-    print(collisions)
-    for result in results:
-        print(result["distance"])
+        dist = []
+        for i in range(len(simulation_result["parkedCorners"])):
+            dist.append(calc_distance(simulation_result["parkedCorners"][i][1], simulation_result["drivingCorners"][i][1]))
+            
+        distances.append(dist)
+
+    for i in range(len(distances)):
+        print(f"#{i}:\t{collisions[i]}:")
+        for j in range(len(distances[i])):
+            print(f"{j}:\t{distances[i][j]}")
     
 main()
 
