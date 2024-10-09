3cc93d9212742361bf9dbce563d3b700b95f9bbd
JACY24 <jordcluitmans@gmail.com>
2024-10-09 12:01:23 +0200

    added typehinting to all methods

--- simulation/simulation.py
+++ simulation/simulation.py
@@ -4,7 +4,6 @@ from scenic.simulators.newtonian import NewtonianSimulator
 from scenic.domains.driving.roads import Network
 import shapely as shapely
 import pandas as pd
-import numpy as np
 
 NUM_SIMULATIONS = 3
 CAR_WIDTH = 2
@@ -13,56 +12,44 @@ SCENARIO = scenic.scenarioFromFile('simulation/test.scenic',
                                     model='scenic.simulators.newtonian.driving_model',
                                     mode2D=True)
 
-def exec_simulation(network = Network.fromFile('Scenic/assets/maps/CARLA/Town05.xodr')):
+def exec_simulation(network: Network = Network.fromFile('Scenic/assets/maps/CARLA/Town05.xodr')) -> dict | None:
     simulator = NewtonianSimulator(network, render=False)
 
     # Execute NUM_SIMULATIONS simulations and evaluate them
     scene, _ = SCENARIO.generate()
     simulation = simulator.simulate(scene, maxIterations = 10)
 
-    if simulation:  # `simulate` can return None if simulation fails
-        # The result of the simulation
-        result = simulation.result
+    result = None
 
-        collided = (False, -1)
-        for timestep, collision in result.records['intersecting']:
-            if collision:
-                collided = (True, timestep)
-                break
+    if simulation:  # `simulate` can return None if simulation fails
+        # The records will be seen as the result of the simulation
+        result = simulation.result.records
 
     simulator.destroy()
+    return result
 
-    return collided, simulation.result.records
-
-def calc_distance(a, b):
+def calc_distance(a: tuple[scenic.core.vectors.Vector], b: tuple[scenic.core.vectors.Vector]) -> float:
     poly_a = shapely.Polygon(a[0:3])
     poly_b = shapely.Polygon(b[0:3])
 
     return round(shapely.distance(poly_a, poly_b), 4)
 
-def format_trace(dist, speed, steps):
+def format_trace(result: dict) -> pd.DataFrame:
+    dist = [calc_distance(result["parkedCorners"][i][1], result["drivingCorners"][i][1]) for i in range(len(result["parkedCorners"]))]
+    speed = [x[1] for x in result["speed"]]
+    
     return pd.DataFrame({
         'Distance': dist,
         'Speed': speed
     })
 
 def main():
-    collisions, distances, speeds = np.array([]), np.array([]), np.array([])
-
     for _ in range(NUM_SIMULATIONS):
-        collided, simulation_result = exec_simulation()
-        np.append(collisions, collided)
-
-        dist = [calc_distance(simulation_result["parkedCorners"][i][1], simulation_result["drivingCorners"][i][1]) for i in range(len(simulation_result["parkedCorners"]))]
-        speed = [x[1] for x in simulation_result["speed"]]
-        
-        trace = format_trace(dist, speed, len(dist))
-
-        np.append(distances, dist)
-        np.append(speeds, speed)
+        simulation_result = exec_simulation()
+        if simulation_result is not None:
+            trace = format_trace(simulation_result)
 
     print(trace)
-
 main()
 
 # DONE:
