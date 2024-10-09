daecbf2b4fcb544e2a7409be6db50c5b3516fb14
JACY24 <jordcluitmans@gmail.com>
2024-10-09 12:01:17 +0200

    Added monitor file

---
+++ simulation/monitor.py
@@ -0,0 +1,20 @@
+import pandas as pd
+
+def monitor(trace: pd.DataFrame) -> bool:
+    """A monitor for the simulation
+
+    Parameters
+    ----------
+    trace : pd.DataFrame 
+            a trace of a system execution
+
+    Returns
+    -------
+    bool:
+            True if the trace cannot be mapped to a trace of the system that violates the specification
+            False otherwise
+    """
+
+    
+
+    return False
